d5539ba83db176b34fc9edf500e3e7b435a0800d
Joshua Boquiren <thejoshbq@proton.me>
2026-06-17 14:30:07 -0400

    Updates

--- site/src/components/layout/CyberpunkGridBackground.tsx
+++
@@ -1,8 +0,0 @@
-export default function CyberpunkGridBackground() {
-  return (
-    <div
-      className="cyberpunk-grid fixed inset-0 pointer-events-none z-0"
-      aria-hidden="true"
-    />
-  )
-}
